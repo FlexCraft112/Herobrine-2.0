43582257b8716b7da53d28b8f4d8c3ef8413f007
FlexCraft112 <159067201+FlexCraft112@users.noreply.github.com>
2026-01-19 03:52:15 +0100

    Update HerobrinePlugin.java

--- src/main/java/me/flexcraft/herobrine/HerobrinePlugin.java
+++ src/main/java/me/flexcraft/herobrine/HerobrinePlugin.java
@@ -1,16 +1,19 @@
 package me.flexcraft.herobrine;
 
+import me.flexcraft.herobrine.command.HerobrineCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HerobrinePlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("Herobrine-2.0 has awakened...");
+        saveDefaultConfig();
+        getCommand("herobrine").setExecutor(new HerobrineCommand(this));
+        getLogger().info("Herobrine-2.0 активирован.");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("Herobrine-2.0 has vanished.");
+        getLogger().info("Herobrine-2.0 отключён.");
     }
 }

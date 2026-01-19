f1f3f18208f7dcad5f064297e0d4434c85f7bcf0
FlexCraft112 <159067201+FlexCraft112@users.noreply.github.com>
2026-01-19 09:45:10 +0100

    Update HerobrineCommand.java

--- src/main/java/me/flexcraft/herobrine/command/HerobrineCommand.java
+++ src/main/java/me/flexcraft/herobrine/command/HerobrineCommand.java
@@ -1,42 +1,29 @@
-package me.flexcraft.herobrine.command;
+@Override
+public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-import me.flexcraft.herobrine.HerobrinePlugin;
-import me.flexcraft.herobrine.npc.HerobrineNPCSpawner;
-import org.bukkit.Bukkit;
-import org.bukkit.command.*;
-import org.bukkit.entity.Player;
-
-public class HerobrineCommand implements CommandExecutor {
-
-    private final HerobrinePlugin plugin;
-
-    public HerobrineCommand(HerobrinePlugin plugin) {
-        this.plugin = plugin;
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-        if (!sender.hasPermission("herobrine.use")) {
-            sender.sendMessage("§cНет прав");
-            return true;
-        }
-
-        if (args.length != 1) {
-            sender.sendMessage("§cИспользование: /herobrine <ник>");
-            return true;
-        }
-
-        Player target = Bukkit.getPlayerExact(args[0]);
-        if (target == null) {
-            sender.sendMessage("§cИгрок не найден");
-            return true;
-        }
-
-        sender.sendMessage("§7Вы призвали §cХеробрина §7для §f" + target.getName());
-        target.sendMessage("§8Вы чувствуете §fчужой взгляд...");
-
-        HerobrineNPCSpawner.spawn(plugin, target);
+    if (!sender.hasPermission("herobrine.use")) {
+        sender.sendMessage("§cНет прав");
         return true;
     }
+
+    if (args.length != 1) {
+        sender.sendMessage("§cИспользование: /herobrine <ник>");
+        return true;
+    }
+
+    if (HerobrineNPCSpawner.isActive()) {
+        sender.sendMessage("§cХеробрин уже рядом... Он ещё не исчез.");
+        return true;
+    }
+
+    Player target = Bukkit.getPlayerExact(args[0]);
+    if (target == null) {
+        sender.sendMessage("§cИгрок не найден");
+        return true;
+    }
+
+    sender.sendMessage("§7Вы призвали §cХеробрина §7для §f" + target.getName());
+    HerobrineNPCSpawner.spawn(plugin, target);
+
+    return true;
 }

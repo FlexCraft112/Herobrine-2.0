9748fbdb5469b5b6cea3c587d193cec2aedee770
FlexCraft112 <159067201+FlexCraft112@users.noreply.github.com>
2026-01-19 06:49:20 +0100

    Update HerobrineCommand.java

--- src/main/java/me/flexcraft/herobrine/command/HerobrineCommand.java
+++ src/main/java/me/flexcraft/herobrine/command/HerobrineCommand.java
@@ -2,14 +2,11 @@ package me.flexcraft.herobrine.command;
 
 import me.flexcraft.herobrine.HerobrinePlugin;
 import me.flexcraft.herobrine.fake.FakeHerobrineSpawner;
-
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
 public class HerobrineCommand implements CommandExecutor {
 
@@ -40,24 +37,18 @@ public class HerobrineCommand implements CommandExecutor {
         }
 
         Player target = Bukkit.getPlayerExact(args[0]);
-
         if (target == null) {
             sender.sendMessage(msg("player-not-found"));
             return true;
         }
 
-        sender.sendMessage(msg("admin-trigger").replace("{player}", target.getName()));
+        sender.sendMessage(
+                msg("admin-trigger").replace("{player}", target.getName())
+        );
         target.sendMessage(msg("target-message"));
 
-        // 👻 ХОРРОР-ЭФФЕКТЫ
-        target.addPotionEffect(new PotionEffect(PotionEffectType.DARKNESS, 60, 0));
-        target.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 40, 1));
-        target.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 100, 0));
-        target.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 60, 2));
-        target.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 80, 1));
-
-        // 🔥 СПАВН ХЕРОБРИНА ПРЯМО ПЕРЕД ИГРОКОМ
-        FakeHerobrineSpawner.spawn(target);
+        // 🔥 ВАЖНО: передаём plugin + target
+        FakeHerobrineSpawner.spawn(plugin, target);
 
         return true;
     }
